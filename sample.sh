25e4b55e756b293da31643af1bc06935ee10786b
gaomusiki <127092852+gaomusiki@users.noreply.github.com>
2025-08-20 20:05:06 +0800

    Update sample.sh

--- sample.sh
+++ sample.sh
@@ -1,6 +1,6 @@
-CUDA_VISIBLE_DEVICES=0 torchrun   --nnodes=1   --nproc_per_node=1  --master_port=29502  sample_copy.py \
+CUDA_VISIBLE_DEVICES=0 torchrun   --nnodes=1   --nproc_per_node=1  --master_port=29506  sample_copy.py \
 --model DiT-L/4 \
---ckpt /data3/xdk/dit-results/001-DiT-L-4/checkpoints/0006000.pt \
+--ckpt /data3/lmy/ss_ksu_result/single_step/checkpoints/0011500.pt \
 --image-size 256 \
---num-classes 1000 \
+--num-classes 10 \
 #--data-path /data0/lmy/imagenet2012/train
